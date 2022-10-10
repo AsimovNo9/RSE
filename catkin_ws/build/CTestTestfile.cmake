84375bf32870ce735314ee5de23b4d6264d0738c
Adedamola Sode <adedamola.sode@gmail.com>
2022-10-10 16:46:01 +0100

    Working with ROS workspaces and catkin packages.

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/xihelm/personal/courses/new/RSE/catkin_ws/src
+# Build directory: /home/xihelm/personal/courses/new/RSE/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("simple_arm")
---
+++ catkin_ws/build/simple_arm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/xihelm/personal/courses/new/RSE/catkin_ws/src/simple_arm
+# Build directory: /home/xihelm/personal/courses/new/RSE/catkin_ws/build/simple_arm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
